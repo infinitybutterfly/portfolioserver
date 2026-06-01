119a89407fe40843886a6310eec3519c63842d59
Ankit Shiv <ragnarok.infinity.butterfly@gmail.com>
2026-06-01 17:37:29 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowReactApp",
         policy =>
         {
-            policy.WithOrigins("http://localhost:3000", "http://localhost:5173")
+            policy.WithOrigins("ServerLinkApp")
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
